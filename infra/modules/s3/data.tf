c85a0362d74d150672a6066c1a63d9ceb60a371b
leobr1t0 <leonardo70177@gmail.com>
2026-02-07 21:08:34 -0300

    removing s3

--- infra/modules/s3/data.tf
+++
@@ -1,8 +0,0 @@
-data "terraform_remote_state" "messaging" {
-  backend = "s3"
-  config = {
-    bucket = "nextime-frame-state-bucket"
-    key    = "messaging/infra.tfstate"
-    region = "us-east-1"
-  }
-}
